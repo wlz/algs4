4da3fe80a4df9ee7aebd9c70af54fd8731a00f2d
Liangzhao Wu <wuliangzhao10@gmail.com>
2014-09-09 13:47:36 +0800

    init .

---
+++ src/c/quick_find.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int seq[10];
+
+void init_seq(); 
+void print_seq();
+void set_union();
+
+int main()
+{
+	init_seq();
+	set_union(1,2);
+	printf("%d\n",connected(1,2));
+	return 0; 
+} 
+
+void set_union(int p, int q)
+{
+	for(int i=0;i<10;i++)
+	{
+		if(seq[i] == seq[q])
+			seq[i] = seq[p];
+	}
+}
+
+int connected(int p, int q)
+{ 
+	return seq[p]==seq[q];
+}
+
+void init_seq()
+{
+	for(int i=0;i<10;i++)
+		seq[i] = i;
+}
+//
+//void print_seq()
+//{
+//	for(int i=0;i<10;i++)
+//	{
+//		printf("%d",seq[i]);
+//		if(i<9)
+//			printf(",");
+//	}
+//
+//	printf("\n");
+//}
---
+++ src/py/quick_find.py
@@ -0,0 +1,31 @@
+cnt = 10
+seq = range(cnt) 
+
+def union(a,b):
+	print 'union:'+str(a)+str(b)
+	aid = seq[a]
+	bid = seq[b]
+	for i in range(len(seq)):
+		if seq[i] == bid:
+			seq[i] = aid
+	print seq
+
+
+def connected(a,b):
+	return True if seq[a]==seq[b] else False
+
+union(4,3)
+union(3,8)
+union(6,5)
+union(9,4)
+union(2,1)
+
+print connected(0,7)
+print connected(8,9)
+
+union(5,0)
+union(7,2)
+union(6,1)
+union(1,0)
+
+print connected(0,7)
