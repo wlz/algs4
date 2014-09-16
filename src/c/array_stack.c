3f1de8d95d9cf6c79dc246eda6e6bf8e0bbd8920
Liangzhao Wu <wuliangzhao10@gmail.com>
2014-09-16 19:59:39 +0800

    add array_stack.c

---
+++ src/c/array_stack.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void push(char* s); 
+char* pop(); 
+int is_empty(); 
+
+char* stack[10]; 
+int point = 0; 
+
+int main()
+{ 
+	push("to"); 
+	push("be"); 
+	push("or"); 
+	push("not"); 
+	push("to"); 
+
+	printf("%s ", pop()); 
+
+	push("be"); 
+	
+	printf("%s ", pop()); 
+	printf("%s ", pop()); 
+
+	push("that"); 
+
+	printf("%s ", pop()); 
+	printf("%s ", pop()); 
+	printf("%s ", pop()); 
+
+	printf("\n"); 
+	push("is");
+
+	return 0; 
+}
+
+int is_empty()
+{
+	return point == 0; 
+}
+
+void push(char* s)
+{ 
+	stack[point++] = s; 
+}
+
+char* pop()
+{
+	return stack[--point]; 
+} 
+
+
+
