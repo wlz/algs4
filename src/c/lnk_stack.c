1b8cd636a40645e69ff51712b40a32279a5868b3
Liangzhao Wu <wuliangzhao10@gmail.com>
2014-09-16 23:21:31 +0800

    add lnk_stack .

---
+++ src/c/lnk_stack.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node node; 
+
+struct Node
+{
+	char* str; 
+	node* last; 
+}; 
+
+void push(char* s); 
+char* pop(); 
+int is_empty(); 
+
+node *first; 
+node *cur; 
+
+int main()
+{ 
+	first = (node*)malloc(sizeof(node)); 
+	first->last = 0; 
+
+	cur = first; 
+
+	push("to"); 
+	push("be"); 
+	push("or"); 
+	push("not"); 
+	push("to"); 
+
+	printf("%s ", pop()); 
+
+	push("be"); 
+
+	printf("%s ", pop()); 
+	printf("%s ", pop()); 
+
+	push("that"); 
+
+	printf("%s ", pop()); 
+	printf("%s ", pop()); 
+	printf("%s ", pop()); 
+
+	push("is"); 
+
+	printf("\n"); 
+
+	return 0; 
+}
+
+void push(char* s) 
+{
+	node* n = (node*)malloc(sizeof(node)); 
+	n->last = cur; 
+	n->str = s; 
+	cur = n; 
+}
+
+char* pop()
+{ 
+	char* s = cur->str; 
+	cur = cur->last; 
+
+	return s; 
+}
+
+int is_empty()
+{ 
+	return cur->last == 0; 
+} 
