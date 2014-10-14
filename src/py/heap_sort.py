69e146d3b3f2deede4c7698c4aad3cf67e6faad9
Liangzhao Wu <wuliangzhao10@gmail.com>
2014-10-14 04:59:33 +0000

    add heap_sort.c .

---
+++ src/py/heap_sort.py
@@ -0,0 +1,35 @@
+from random import randrange
+
+def gen_data():
+    seq = []
+    for i in range(10):
+        seq.append(i)
+    for i in range(1, 10):
+        r = randrange(i)
+        seq[i], seq[r] = seq[r], seq[i]
+    return seq
+
+def heap_sort(seq):
+    for i in range((len(seq) - 1)/ 2, -1, -1):
+        sink(seq, i, len(seq))
+
+    cnt = len(seq)
+    while cnt > 0:
+        seq[0], seq[cnt - 1] = seq[cnt - 1], seq[0]
+        cnt = cnt - 1
+        sink(seq, 0, cnt)
+
+def sink(seq, k, n):
+    while (k + 1) * 2 - 1 < n:
+        j = (k + 1) * 2 - 1
+        if j < n - 1 and seq[j] < seq[j + 1]:
+            j = j + 1
+        if seq[k] > seq[j]: 
+            break
+        seq[j], seq[k] = seq[k], seq[j]
+        k = j
+
+seq = gen_data()
+heap_sort(seq)
+
+print seq
