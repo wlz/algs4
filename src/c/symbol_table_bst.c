ebafc6786bb81deedb1d28acbd7909c5ceaa61ca
Liangzhao Wu <wuliangzhao10@gmail.com>
2014-10-18 12:44:51 +0000

    bst add max and min .

--- src/c/symbol_table_bst.c
+++ src/c/symbol_table_bst.c
@@ -15,9 +15,14 @@ static node* root = NULL;
 
 void put(int key, char* value);
 char* get(int key);
+void delete(int key);
 int contains(int key);
 int is_empty();
 int size();
+char* max();
+char* min();
+int floor(int key);
+int ceiling(int key);
 
 node* create_node(int key, char* value);
 node* put_node(node* n, int key, char* value);
@@ -29,11 +34,12 @@ int size_node(node* n);
 
 int main()
 {
-    put(1, "aaa");
     put(3, "bbb");
+    put(1, "aaa");
     put(2, "ccc");
     put(2, "222");
     put(4, "mmm");
+    put(5, "nnn");
 
     printf("%s\n", get(2));
     printf("%d\n", contains(1));
@@ -42,8 +48,35 @@ int main()
     printf("%d\n", is_empty());
     printf("%d\n", size()); 
 
+    printf("%s\n", max()); 
+    printf("%s\n", min()); 
+
     print();
     return 0;
+}
+
+char* max()
+{
+    return max_node(root);
+}
+
+char* max_node(node* n)
+{
+    if(!n) return NULL;
+    if(!n->right) return n->value;
+    else return max_node(n->right);
+}
+
+char* min()
+{
+    return min_node(root);
+}
+
+char* min_node(node* n)
+{
+    if(!n) return NULL;
+    if(!n->left) return n->value;
+    else return min_node(n->left);
 }
 
 int size()
