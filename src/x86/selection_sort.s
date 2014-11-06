42d0a4c32a84387b4b14a49904ee0c4484a3776b
Liangzhao Wu <wuliangzhao10@gmail.com>
2014-11-06 15:01:05 +0800

    finish selection_sort.s .

--- src/x86/selection_sort.s
+++ src/x86/selection_sort.s
@@ -12,12 +12,69 @@ main:
     movl    %esp, %ebp
     subl    $16, %esp 
     
-    movl    $0, %eax 
-
     call    init
     call    print 
 
+    call    select_sort
+    call    print
+
+    movl    $0, %eax 
     leave
+    ret
+
+select_sort:
+    pushl   %ebp
+    movl    %esp, %ebp
+    subl    $16, %esp 
+
+    movl    $0, -4(%ebp)
+
+loop_swap:
+    movl    -4(%ebp), %eax
+    movl    %eax, (%esp)
+    call    get_min
+    
+    movl    %eax, (%esp)
+    movl    -4(%ebp), %eax
+    movl    %eax, 4(%esp) 
+    call    swap
+
+    movl    -4(%ebp), %eax
+    addl    $1, %eax
+    movl    %eax, -4(%ebp)
+
+    movl    N, %ebx
+    cmpl    %eax, %ebx
+    jg      loop_swap 
+
+    leave
+    ret
+
+get_min: 
+    pushl   %ebp
+    movl    %esp, %ebp
+    subl    $16, %esp 
+    
+    movl    8(%ebp), %eax
+    movl    %eax, -4(%ebp) 
+    movl    %eax, %edi
+    
+loop_cmp:
+    movl    -4(%ebp), %eax
+    movl    seq(, %eax, 4), %eax    
+    movl    seq(, %edi, 4), %ebx 
+    cmpl    %eax, %ebx
+    jg      loop_next
+    movl    %edi, -4(%ebp) 
+
+loop_next: 
+    addl    $1, %edi
+    movl    N, %eax     
+    cmpl    %edi, %eax 
+    jg      loop_cmp
+    
+    movl    -4(%ebp), %eax 
+    leave 
     ret
 
 print:
