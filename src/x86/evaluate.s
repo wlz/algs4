2c4469e818673cf703c65a3a0954abd7ac0a2f30
Liangzhao Wu <wuliangzhao10@gmail.com>
2014-11-11 20:10:08 +0800

    finish evaluate.s .

--- src/x86/evaluate.s
+++ src/x86/evaluate.s
@@ -1,169 +1,108 @@
-    .data   
-    .comm   value_stack,    40
-    .comm   operator_stack, 10
+    .data
+    .comm   val_stack,    40
+    .comm   op_stack,     10
 val_loc:
     .long   0
 op_loc:
-    .long   0
-fmt_d:
-    .string "%d "
+    .long   0 
 fmt_c:
-    .string "%c"
-fmt_x:
-    .string "%x "
-line:
-    .string "\n"
+    .string "%c|"
+fmt_d:
+    .string "%d|" 
 exp:
-    .string "(1 + 2)"
-#    .string "(1 + (2 * 3))"
+    .string "(1+((2 + 3)*(4*5)))"
 
-    .globl main
+    .globl  main
 main:
     pushl   %ebp
     movl    %esp, %ebp
-    subl    $24, %esp 
+    subl    $16, %esp
 
     call    calculate
 
+    movl    val_stack, %eax
+    movl    %eax, 4(%esp)
+    movl    $fmt_d, (%esp)
+    call    printf
+
+    call    calculate
+    movl    $0, %eax    
     leave
     ret
-
-#    printf("%d\n", '+'); //43
-#    printf("%d\n", '*'); //42
-#    printf("%d\n", ' '); //32
-#    printf("%d\n", '('); //40
-#    printf("%d\n", ')'); //41
 
 calculate:
     pushl   %ebp
     movl    %esp, %ebp
     subl    $16, %esp
 
-    movl    $0, %eax
-    movl    %eax, -12(%ebp)
-
-move_next:   
-    movl    -12(%ebp), %ebx 
-    movb    exp(, %ebx, 1), %al    
-
-#    cmpb    $0, %al     # '\0'
-#    cmpl    $0, %eax     # '\0'
+    movl    $exp, %edi 
+calc_next:
+    movb    (%edi), %al 
     cmpb    $0, %al
-    je      calc_done
+    je      calc_exit
 
-    movb    %al, 4(%esp)
-    movl    $fmt_c, (%esp)
-    call    printf
-    jmp     next
+    cmpb    $40, %al    # (
+    je      move_next
 
-#    cmpb    $40, %al    # '('
-    cmpl    $40, %eax    # '('
-    je      next
+    cmpb    $32, %al    # ' '
+    je      move_next
 
-#    cmpb    $32, %al    # ' '
-    cmpl    $32, %eax    # ' '
-    je      next
+    cmpb    $42, %al    # *
+    je      add_op
 
-#    cmpb    $43, %al    # '+'
-    cmpl    $43, %eax    # '+'
-    je      push_add
+    cmpb    $43, %al    # +
+    je      add_op
 
-#    cmpb    $42, %al    # '*'
-    cmpl    $42, %eax    # '*'
-    je      push_multi 
-
-#    cmpb    $41, %al    # ')'
-    cmpl    $41, %eax    # ')'
+    cmpb    $41, %al
     je      calc
 
-    subl    $48, %eax
+    subb    $48, %al
+    movsbl  %al, %eax
     movl    %eax, (%esp)
-
     call    push_val
-    jmp     next
+    jmp     move_next
 
 calc:
     call    pop_val
     movl    %eax, -4(%ebp)
     call    pop_val
     movl    %eax, -8(%ebp)
-    call    pop_op
+    call    pop_op 
 
-    cmpb    $43, %al    # '+'
+    cmpb    $43, %al    # +
     je      calc_add
 
-    cmpb    $42, %al    # '*'
+    cmpb    $42, %al    # *
     je      calc_multi
 
 calc_add:
-    movl    -4(%ebp), %eax
-    movl    -8(%ebp), %ebx
-    addl    %eax, %ebx
-    movl    %ebx, (%esp)
-    call    push_val
-
-#    movl    %ebx, 4(%esp)
-#    movl    $fmt_d, (%esp)
-#    call    printf
-
-    jmp     next
-
-calc_multi:
-    movl    -4(%ebp), %eax
-    movl    -8(%ebp), %ebx
-    imull   %eax, %ebx
-    movl    %ebx, (%esp)
-    call    push_val
-    jmp     next
-    
-push_add:
-    movb    $43, (%esp)
-    call    push_op
-    jmp     next
-
-push_multi:
-    movb    $42, (%esp)
-    call    push_op
-    jmp     next
-
-next:
-    movl    -12(%ebp), %eax
-    addl    $1, %eax 
-    movl    %eax, -12(%ebp)
-
-    movl    %eax, 4(%esp)
-    movl    $fmt_d, (%esp)
-    call    printf
-
+    movl    -4(%ebp), %ebx
+    movl    -8(%ebp), %eax
+    addl    %ebx, %eax
+    movl    %eax, (%esp)
+    call    push_val 
     jmp     move_next
 
-calc_done:
-    leave
-    ret
+calc_multi:
+    movl    -4(%ebp), %ebx
+    movl    -8(%ebp), %eax
+    imull   %ebx, %eax
+    movl    %eax, (%esp)
+    call    push_val 
+    jmp     move_next
 
-push_val:
-    pushl   %ebp
-    movl    %esp, %ebp
-    subl    $16, %esp
+add_op:
+    movsbl  %al, %eax
+    movl    %eax, (%esp)
+    call    push_op
 
-    movl    8(%ebp), %eax 
-    movl    val_loc, %edi
-    movl    %eax, value_stack(, %edi, 4)
+    jmp     move_next 
+
+move_next: 
     addl    $1, %edi
-    movl    %edi, (val_loc)
-    leave
-    ret
+    jmp     calc_next
 
-pop_val:
-    pushl   %ebp
-    movl    %esp, %ebp
-    subl    $16, %esp
-
-    movl    val_loc, %edi
-    subl    $1, %edi
-    movl    %edi, (val_loc)
-
-    movl    value_stack(, %edi, 4), %eax 
+calc_exit:
     leave
     ret
 
@@ -173,42 +112,67 @@ push_op:
     subl    $16, %esp
 
     movb    8(%ebp), %al
-    movl    op_loc, %edi
-    movb    %al, operator_stack(, %edi, 1)
-    addl    $1, %edi
-    movl    %edi, (op_loc) 
+
+#    movsbl  %al, %eax
+#    movl    %eax, 4(%esp)
+#    movl    $fmt_c, (%esp)
+#    call    printf 
+
+    movl    op_loc, %ebx
+    movb    %al, op_stack(, %ebx, 1)
+    addl    $1, %ebx
+    movl    %ebx, op_loc 
+    
     leave
-    ret 
+    ret
 
 pop_op:
+    movl    op_loc, %eax
+    subl    $1, %eax
+    movl    %eax, op_loc
+    movb    op_stack(, %eax, 1), %al
+    movsbl  %al, %eax   
+    ret
+
+push_val:
     pushl   %ebp
     movl    %esp, %ebp
     subl    $16, %esp
-
-    movl    op_loc, %edi
-    subl    $1, %edi
-    movl    %edi, (op_loc)
-
-    movb    operator_stack(, %edi, 1), %al    
+    
+    movl    8(%ebp), %eax    
+    movl    val_loc, %ebx
+    movl    %eax, val_stack(, %ebx, 4)
+    addl    $1, %ebx
+    movl    %ebx, val_loc
     leave
+    ret
+
+pop_val:
+    movl    val_loc, %eax
+    subl    $1, %eax
+    movl    %eax, val_loc
+    movl    val_stack(, %eax, 4), %eax
     ret
 
 display:
     pushl   %ebp
     movl    %esp, %ebp
     subl    $16, %esp
-
     movl    $0, %edi
-show_next:
-    movl    exp(, %edi, 1), %eax
-    movl    %eax, 4(%esp)
-    movl    $fmt_c, (%esp)
-    call    printf
-    
-    addl    $1, %edi
-    movb    exp(, %edi, 1), %al
-    testb   %al, %al
-    jne     show_next
 
+display_next:
+    movl    $0, %eax 
+    movb    exp(, %edi, 1), %al
+    cmpb    $0, %al
+    je      display_exit 
+
+    movsbl    %al, %eax
+    movl    %eax, 4(%esp)
+    movl    $fmt_d, (%esp)
+    call    printf
+    addl    $1, %edi
+    jmp     display_next
+
+display_exit:
     leave
     ret
