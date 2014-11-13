51b76416d6365dd25369525a61a5c4a4228d00ce
Liangzhao Wu <wuliangzhao10@gmail.com>
2014-11-14 00:15:02 +0800

    finish merge feature .

--- src/x86/merge_sort_bu.s
+++ src/x86/merge_sort_bu.s
@@ -14,10 +14,132 @@ main:
     movl    %esp, %ebp
     subl    $16, %esp
 
-    call    init
+#    call    init
+    call    set_test
+#    call    display
+
+    movl    $0, 8(%esp)
+    movl    $4, 4(%esp)
+    movl    $9, (%esp)
+
+    call    merge
     call    display
 
     movl    $0, %eax
+    leave
+    ret
+
+set_test:
+    movl    $1, seq
+    movl    $3, seq+4
+    movl    $5, seq+8
+    movl    $6, seq+12
+    movl    $7, seq+16
+    movl    $0, seq+20
+    movl    $2, seq+24
+    movl    $4, seq+28
+    movl    $8, seq+32
+    movl    $9, seq+36
+    ret
+
+merge:
+    pushl   %ebp
+    movl    %esp, %ebp
+    subl    $40, %esp
+
+    movl    16(%ebp), %eax
+    movl    %eax, -4(%ebp)  # lo
+    movl    %eax, -16(%ebp) # i 
+    movl    %eax, -20(%ebp) # j 
+
+    movl    12(%ebp), %eax
+    movl    %eax, -8(%ebp)  # mid
+    addl    $1, %eax
+    movl    %eax, -24(%ebp) # k
+
+    movl    8(%ebp), %eax
+    movl    %eax, -12(%ebp) # hi
+
+    movl    -12(%ebp), %eax
+    movl    %eax, (%esp)
+
+    movl    -4(%ebp), %eax
+    movl    %eax, 4(%esp)
+
+    call    copy
+
+merge_next:
+    movl    -20(%ebp), %eax
+    movl    -8(%ebp), %ebx
+    cmpl    %ebx, %eax
+    jg      add_right
+
+    movl    -24(%ebp), %eax
+    movl    -12(%ebp), %ebx
+    cmpl    %ebx, %eax
+    jg      add_left
+
+    movl    -20(%ebp), %eax
+    movl    -24(%ebp), %ebx
+    movl    aux(, %eax, 4), %eax
+    movl    aux(, %ebx, 4), %ebx
+    
+    cmpl    %ebx, %eax
+    jg      add_right
+
+    jmp     add_left
+
+add_left:
+
+    movl    -20(%ebp), %eax
+    movl    aux(, %eax, 4), %ebx
+    addl    $1, %eax
+    movl    %eax, -20(%ebp)
+    movl    -16(%ebp), %eax
+    movl    %ebx, seq(, %eax, 4)
+
+    jmp     move_next
+
+add_right:
+
+    movl    -24(%ebp), %eax
+    movl    aux(, %eax, 4), %ebx
+    addl    $1, %eax
+    movl    %eax, -24(%ebp)
+    movl    -16(%ebp), %eax
+    movl    %ebx, seq(, %eax, 4)
+
+    jmp     move_next
+
+move_next:
+
+    movl    -16(%ebp), %eax
+    addl    $1, %eax
+    movl    %eax, -16(%ebp)
+    movl    -12(%ebp), %ebx
+    cmpl    %eax, %ebx
+    jge     merge_next
+    
+merge_done:
+    leave
+    ret 
+
+copy:
+    pushl   %ebp
+    movl    %esp, %ebp
+    subl    $16, %esp
+
+    movl    12(%ebp), %edi
+    movl    8(%ebp), %esi
+
+copy_next:
+    movl    seq(, %edi, 4), %eax
+    movl    %eax, aux(, %edi, 4)
+    addl    $1, %edi
+
+    cmpl    %edi, %esi
+    jge      copy_next 
+    
     leave
     ret
 
@@ -115,6 +237,35 @@ display_next:
     addl    $1, %edi
     cmpl    %edi, %esi
     jg      display_next 
+    
+    movl    $line, %eax
+    movl    %eax, (%esp)
+    call    printf
+    
+    leave
+    ret
+
+display_aux:
+    pushl   %ebp
+    movl    %esp, %ebp
+    subl    $16, %esp
+
+    movl    N, %esi
+    movl    $0, %edi 
+
+display_aux_next:
+    movl    aux(, %edi, 4), %eax
+    movl    %eax, 4(%esp)
+    movl    $fmt_d, (%esp)
+    call    printf
+
+    addl    $1, %edi
+    cmpl    %edi, %esi
+    jg      display_aux_next 
+    
+    movl    $line, %eax
+    movl    %eax, (%esp)
+    call    printf
     
     leave
     ret
