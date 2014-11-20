9379b1ea90e9b58486de27db847649d688d322f0
Liangzhao Wu <wuliangzhao10@gmail.com>
2014-11-20 21:19:52 +0800

    finish simple_bst.s .

--- src/x86/simple_bst.s
+++ src/x86/simple_bst.s
@@ -3,7 +3,7 @@
 fmt_d:
     .string "%d\n"
 line:
-    .string "\n"
+    .string "----------------\n"
 
     .globl  main
 main:
@@ -11,13 +11,19 @@ main:
     movl    %esp, %ebp
     subl    $16, %esp
 
+    movl    $5, (%esp)
+    call    put
     movl    $3, (%esp)
     call    put
-
-    movl    $2, (%esp)
+    movl    $7, (%esp)
     call    put
-
     movl    $1, (%esp)
+    call    put
+    movl    $4, (%esp)
+    call    put
+    movl    $6, (%esp)
+    call    put
+    movl    $9, (%esp)
     call    put
 
     movl    root, %eax
@@ -28,8 +34,14 @@ main:
     call    printf
 
     movl    root, %eax
-    movl    %eax, (%esp)
-    call    del_min
+    movl    %eax, 4(%esp)
+    movl    $3, (%esp)
+    call    delete
+
+    movl    root, %eax
+    movl    %eax, 4(%esp)
+    movl    $7, (%esp)
+    call    delete
 
     movl    %eax, root
 
@@ -44,25 +56,90 @@ main:
 delete:
     pushl   %ebp
     movl    %esp, %ebp
-    subl    $40, %esp 
+    subl    $16, %esp
 
     movl    12(%ebp), %eax
     cmpl    $0, %eax
-    je      delete_exit
+    je     delete_exit 
     
-    movl    (%eax), %eax 
-    movl    8(%ebp), %ebx
-    cmpl    %eax, %ebx 
+    movl    (%eax), %eax
 
+    movl    8(%ebp), %ebx
+    cmpl    %eax, %ebx
+    
+    je      del_itself
     jg      del_right
     jl      del_left
-    
+
+del_itself:
+    movl    12(%ebp), %eax
+    movl    4(%eax), %ebx
+    cmpl    $0, %ebx
+    je      return_right
+
+    movl    8(%eax), %ebx
+    cmpl    $0, %ebx 
+    je      return_left
+
+    movl    12(%ebp), %eax
+    movl    %eax, -4(%ebp)
+    movl    8(%eax), %eax
+    movl    %eax, (%esp)
+    call    get_min
+    movl    %eax, -4(%ebp)
+
+    movl    12(%ebp), %ebx
+    movl    4(%ebx), %eax
+    movl    -4(%ebp), %ebx
+    movl    %eax, 4(%ebx)
+
+    movl    -4(%ebp), %eax
+    movl    8(%eax), %eax
+    movl    %eax, (%esp)
+    call    del_min
+    movl    -4(%ebp), %ebx
+    movl    %eax, 8(%ebx)
+
+    movl    -4(%ebp), %eax
+    jmp     delete_exit 
+
+return_left:
+    movl    4(%eax), %eax
+    jmp     delete_exit
+return_right:
+    movl    8(%eax), %eax 
+    jmp     delete_exit
+
 del_right:
+    #   n->right = del(n->right, data)
+    movl    12(%ebp), %eax
+    movl    8(%eax), %eax
+    movl    %eax, 4(%esp)
+    movl    8(%ebp), %eax
+    movl    %eax, (%esp)
+    
+    call    delete 
+    movl    12(%ebp), %ebx
+    movl    %eax, 8(%ebx)
+
+    movl    12(%ebp), %eax 
     jmp     delete_exit
 
 del_left:
-    jmp     delete_exit
+    #   n->left = del(n->left, data)
+    movl    12(%ebp), %eax
+    movl    4(%eax), %eax
+    movl    %eax, 4(%esp)
+    movl    8(%ebp), %eax
+    movl    %eax, (%esp)
 
+    call    delete
+    movl    12(%ebp), %ebx
+    movl    %eax, 4(%ebx)
+    
+    movl    12(%ebp), %eax 
+    jmp     delete_exit
+    
 delete_exit:
     leave
     ret 
