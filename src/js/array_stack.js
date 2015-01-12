d8e182c09cfe8cbec173bbb6115d8ede56265c84
Liangzhao Wu <wuliangzhao10@gmail.com>
2015-01-12 19:41:22 +0800

    add array_stack.js .

---
+++ src/js/array_stack.js
@@ -0,0 +1,39 @@
+var stack = []; 
+var point = 0;
+
+function push(val)
+{ 
+    stack[point++] = val;
+}
+
+function pop() 
+{
+    return stack[--point];
+}
+
+function is_empty()
+{
+    return point == 0;
+}
+
+push("to");
+push("be");
+push("or");
+push("not");
+push("to");
+
+console.log(pop());
+
+push("be");
+
+console.log(pop());
+console.log(pop());
+
+push("that");
+
+console.log(pop());
+console.log(pop());
+console.log(pop());
+
+console.log("\n");
+push("is");
